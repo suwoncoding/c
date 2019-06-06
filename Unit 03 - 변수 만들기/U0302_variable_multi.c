c3e384588e8a3d42389e5b0090c914eb123173a2
suwoncoding <suwoncoding@gmail.com>
2019-06-06 23:46:05 +0900

    Add files via upload

---
+++ Unit 03 - 변수 만들기/U0302_variable_multi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// 변수를 여러개 선언하고 값을 할당하기 
+
+int main()
+{
+    int num1, num2, num3;    // 변수를 콤마로 구분하여 변수 여러 개를 선언
+
+    num1 = 10;    // 변수에 값 할당
+    num2 = 20;
+    num3 = 30;
+
+    printf("%d %d %d\n", num1, num2, num3);    // 10 20 30: 변수에 저장된 값을 %d로 출력
+
+    return 0;
+}
